d8c4541d2b0d6f04f3af375f004af50db1b4f4ae
Michael James <mijames@vmware.com>
2022-07-05 21:36:50 -0400

    Initial commit.

--- 23-dkube-install.sh
+++ 23-dkube-install.sh
@@ -6,6 +6,10 @@ vim values.yaml
 
 helm install -f values.yaml tanzu-dkube dkube-helm/dkube-deployer
 
+sleep 15
+
 kubectl wait --for=condition=ready --timeout=5m pod -l job-name=dkube-helm-installer
 kubectl logs -l job-name=dkube-helm-installer --follow --tail=-1 && kubectl wait --for=condition=complete --timeout=30m job/dkube-helm-installer
 
+kubectl patch svc istio-ingressgateway -n istio-system  -p '{"spec":{"type":"LoadBalancer"}}'
+kubectl -n istio-system get svc istio-ingressgateway
